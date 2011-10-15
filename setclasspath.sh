ed47242780b4434dda503deff18049ef24a39903
Jeosadache Galvão <josa.galvao@gmail.com>
2011-10-14 22:41:50 -0300

    corrigido o clashpaht acho que definitivamente

--- setclasspath.sh
+++ setclasspath.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+GRAILS_VERSION="2.0.0.M2"
 rm -fv .tmp
 rm -fv .classpath.final
 
 echo '<?xml version="1.0" encoding="UTF-8"?>' > .classpath.final
 echo '<classpath>' >> .classpath.final
-cat .classpath | grep classpathentry | egrep -v  'classpathentry kind="lib"' >> .classpath.final
+cat .classpath | grep classpathentry | egrep -v  'classpathentry kind="lib"' | egrep -v plugin-classes >> .classpath.final
+echo '	<classpathentry kind="lib" path="'$HOME'/.grails/'$GRAILS_VERSION'/projects/'${PWD##*/}'/plugin-classes"/>' >>	 .classpath.final
 
-find ~/.grails/ivy-cache/  -type f -name "*.jar" | xargs -0 -I entry echo entry | grep jar >> .tmp
+find ~/.grails/ivy-cache/  -type f -name "*.jar" | xargs -0 -I entry echo entry | grep jar >> .tmp 
 while read line    
 do
-	echo -e "	<classpathentry kind=\"lib\" path=\"$line\"/>" >> .classpath.final
+  echo -e "	<classpathentry kind=\"lib\" path=\"$line\"/>" >> .classpath.final
 done < .tmp
 
 echo '</classpath>' >> .classpath.final
